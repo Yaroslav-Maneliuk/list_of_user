caf15e7700a0d3065a65894c07503f9916972550
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-12-28 18:27:41 +0200

    Complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import * as ReactDOMClient from "react-dom/client";
+import App from "./App"
+import "./css/main.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+app.render(<App />);
